31bf6acee681cea9d8f0c2f14cbeb1fd2ef9a601
jesade-vbg <jesade.vbg@gmail.com>
2024-05-15 16:45:48 +0200

    Added legacy proxies and backend

---
+++ backend/mapservice/Models/Config/WMSLayerLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MapService.Models.Config
+{
+    public class WMSLayerLayer
+    {
+        public string style { get; set; }
+        public string name { get; set; }
+    }
+}
